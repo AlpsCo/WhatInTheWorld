2b6446810488dca7a63b86306394983b15c503e2
JP Keith <jpkeith15@gmail.com>
2022-11-15 10:18:11 -0800

    Co-authored-by: Brian P <brianhip@users.noreply.github.com> Co-authored-by: tony diethelm <tonydiethelm@gmail.com>

--- client/App.jsx
+++ client/App.jsx
@@ -1,2 +1,20 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Gamepage from './componenets/Gamepage';
+
+function App() {
+  return (
+    <div>
+      <Routes>
+        <Route path='/' element={<Login />} />
+        <Route path='/signup' element= {<Signup />} />
+        <Route path='/gamepage' element= {<Gamepage />} />
+      </Routes>
+    </div>
+  );
+}
+
+
+export default App;
