acfd43946b6ee7ba024419897ae81c57bb21f9c2
zihaoli518 <zihaoli518@gmail.com>
2022-11-14 23:50:40 -0800

    fixed an error with react and added some boiler code

--- client/App.jsx
+++ client/App.jsx
@@ -1,2 +1,16 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import Main from './components/Main'
+
+const App = () => {
+
+  return(
+    <div> 
+      <h3>If you can see this, react is working. yay!</h3> 
+      <Main />
+    </div>
+  )
+}
+
+
+export default App
---
+++ client/components/Main.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+const Main = () => {
+
+  const onClick = () => {
+    // send get request to /encyclopedia for getting array of countries 
+  }
+
+  return (
+    <div className='main'>
+      <div className='top-bar'> 
+        <button>encylopedia</button>
+        <button>Logout</button>
+      </div>
+      <div className='score-timer'>
+
+      </div>
+      <div className='country-fact'>
+
+      </div>
+      <div className='UI-buttons'>
+
+      </div>
+    </div>
+  )
+}
+
+export default Main
