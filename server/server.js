440c13f6cad4e58f68ef30c0e750c24e0cf3dee7
hbernie <hbern00@gmail.com>
2022-11-14 22:51:42 -0800

    signup and login functionality complete with bcrypt

--- server/server.js
+++ server/server.js
@@ -1,7 +1,10 @@
-const express = require("express");
+const express = require('express');
 const app = express();
-const path = require("path");
+const path = require('path');
 const PORT = 3000;
+
+const userController = require('./controllers/userController');
+
 
 app.use(express.json());
 
@@ -9,20 +12,28 @@ app.use(express.json());
 // app.use('/', express.static(path.resolve(__dirname, '../assets')));
 
 // serving bundle.js in ../dist
-app.use('/dist', express.static(path.join(__dirname, '../dist')));
+// app.use('/dist', express.static(path.join(__dirname, '../dist')));
 
-
-
-app.get("/",
-(req, res) => {
+app.get('/', (req, res) => {
+  console.log('here i am');
   res.status(200).sendFile(path.join(__dirname, '../client/index.html'));
-})
+});
+
+app.post('/signup', userController.signUp, (req, res) => {
+  console.log('Signed up successfully');
+  return res.status(200).json();
+});
+
+app.post('/login', userController.logIn, (req, res) => {
+  console.log('Logged in successfully');
+  return res.status(200).json(res.locals.username);
+});
 
 
-
-
+// catch-all route handler for any requests to an unknown route
+app.use((req, res) => res.sendStatus(404));
 
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}...`);
-})
+});
